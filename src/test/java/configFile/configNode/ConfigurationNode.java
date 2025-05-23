f64ea3a03dad005780b999e4d8bcc9720decfecb
cristian15mn <cristianmarius.negrea@gmail.com>
2025-05-23 11:56:26 +0300

    Implement xml file on local for config

---
+++ src/test/java/configFile/configNode/ConfigurationNode.java
@@ -0,0 +1,13 @@
+package configFile.configNode;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="configuration")
+public class ConfigurationNode {
+
+    @XmlElement(name = "DriverConfig")
+    public DriverConfigNode driverConfigNode;
+//    @XmlElement(name="DatabaseConfig")
+//    public DatabaseConfigNode databaseConfigNode;
+}
--- src/test/java/demoQAWebSite/ShareData/ShareData.java
+++ src/test/java/demoQAWebSite/ShareData/ShareData.java
@@ -1,5 +1,7 @@
 package demoQAWebSite.ShareData;
 
+import configFile.ConfigFile;
+import configFile.configNode.ConfigurationNode;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -14,14 +16,15 @@ public class ShareData {
     @BeforeMethod
     public void prepareBrowser(){
 
+        ConfigurationNode configurationNode= ConfigFile.createConfigNode(ConfigurationNode.class);
+
         //deschidem un browser de Chrome
         driver = new ChromeDriver();
 
-        //accesam o pagina web
-        driver.get("https://demoqa.com/");
-
         //facem browser-ul in modul maximize
         driver.manage().window().maximize();
+        //accesam o pagina web
+        driver.get(configurationNode.driverConfigNode.url);
 
         //Definim un wait implicit pentru un interval maxim de timp
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
